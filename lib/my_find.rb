a8b745f74a098b07afd4a40175469c7a54cea1b8
Steph W <wilhelm.steph@gmail.com>
2020-04-08 22:14:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,12 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+    if yield(collection[i])
+      return(collection[i])
+    end
     i = i + 1
   end
-  if collection.find(false)
-      false
-    else
-      true
-    end
 end
